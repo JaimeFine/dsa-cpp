883872aa708fab996e5bdfa194b21c7113eb7201
JaimeFine <jaimefine6@gmail.com>
2026-05-02 14:37:18 +0800

    New quiz

---
+++ homeworks/7.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<string, int> users;
+
+    users["tourist"] = 3858;
+    users["ksun48"] = 3679;
+    users["Benq"] = 3658;
+    users["Um_nik"] = 3648;
+    users["apiad"] = 3638;
+    users["Stonefeang"] = 3630;
+    users["ecnerwala"] = 3613;
+    users["mnbvmar"] = 3555;
+    users["newbiedmy"] = 3516;
+    users["semiexp"] = 3481;
+
+    string name;
+    cin >> name;
+    cout << users[name] << endl;
+}
+
